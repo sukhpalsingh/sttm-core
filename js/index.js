7aae2758b35ce8ad9c0f6606eef76c24e43564ef
Navdeep Singh <info@cyfersystems.com>
2016-04-21 22:56:37 -0700

    Load sqlite DB with SQL.js library - **This may be abstracted later to use electron/cordova-specific code if it's faster** Basic search first-letter-anywhere-style Don't track CSS map

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var db;
+
+var xhr = new XMLHttpRequest();
+console.log("xhr.open");
+xhr.open('GET', '../database.sqlite', true);
+xhr.responseType = 'arraybuffer';
+
+xhr.onload = function(e) {
+  var uInt8Array = new Uint8Array(this.response);
+  db = new SQL.Database(uInt8Array);
+  //var contents = db.exec("SELECT DISTINCT source_id FROM shabad");
+  //console.dir(contents);// contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+};
+xhr.send();
+
+var $search = document.getElementById("search");
+var $results = document.getElementById("results");
+
+function search() {
+  var search_query = $search.value;
+  if (search_query.length > 2) {
+    $results.innerHTML = "";
+    var content = db.exec("SELECT ID, Gurmukhi, ShabadID FROM Shabad WHERE FirstLetters LIKE '%" + search_query + "%'");
+    if (content.length > 0) {
+      content[0].values.forEach(function(item, i) {
+        $results.innerHTML = $results.innerHTML + "<div><a href='#'>" + item[1] + "</a></div>";
+      });
+    }
+  }
+}
