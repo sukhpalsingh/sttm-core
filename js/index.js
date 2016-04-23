89affe1f5c76a68ad6370475297a7c39d3add820
Navdeep Singh <info@cyfersystems.com>
2016-04-22 17:53:25 -0700

    Add Shabad to session on result click Display full Shabad in list

--- js/index.js
+++ js/index.js
@@ -12,9 +12,13 @@ xhr.onload = function(e) {
 xhr.send();
 
 var $search = document.getElementById("search");
+var $shabad = document.getElementById("shabad");
 var $results = document.getElementById("results");
+var $session = document.getElementById("session");
 
 $search.addEventListener("keyup", typeSearch);
+$results.addEventListener("click", clickResult);
+$session.addEventListener("click", clickSession);
 
 function typeSearch() {
   clearTimeout(new_search_timeout);
@@ -28,12 +32,39 @@ function search() {
     if (content.length > 0) {
       $results.innerHTML = "";
       content[0].values.forEach(function(item, i) {
-        $results.innerHTML = $results.innerHTML + "<li><a href='#'>" + item[1] + "</a></li>";
+        $results.innerHTML = $results.innerHTML + "<li><a href='#' class='panktee' data-shabad-id='" + item[2] + "'>" + item[1] + "</a></li>";
       });
     } else {
       $results.innerHTML = "<li class='english'>No results.</li>";
     }
   } else {
     $results.innerHTML = "";
+  }
+}
+
+function clickResult(e) {
+  if (e.target.classList.contains("panktee")) {
+    var $panktee = e.target;
+    var ShabadID = $panktee.dataset.shabadId;
+    $session.innerHTML = $session.innerHTML + '<li><a href="#" class="panktee" data-shabad-id="' + ShabadID + '">' + $panktee.innerText + '</a></li>';
+    loadShabad(ShabadID);
+  }
+}
+
+function clickSession(e) {
+  if (e.target.classList.contains("panktee")) {
+    var $panktee = e.target;
+    var ShabadID = $panktee.dataset.shabadId;
+    loadShabad(ShabadID);
+  }
+}
+
+function loadShabad(ShabadID) {
+  var content = db.exec("SELECT ID, Gurmukhi FROM Shabad WHERE ShabadID = '" + ShabadID + "'");
+  if (content.length > 0) {
+    $shabad.innerHTML = "";
+    content[0].values.forEach(function(item, i) {
+      $shabad.innerHTML = $shabad.innerHTML + '<li><a href="#" class="panktee">' + item[1] + '</a></li>';
+    });
   }
 }
