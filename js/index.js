9993b2b05f158599cd43cf98308a5d1d40c786dd
Navdeep Singh <info@cyfersystems.com>
2016-09-02 14:58:53 -0700

    Retain structure in in medium size Use fallback functions when testing this repo by itself

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
 var db,
     new_search_timeout;
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '../gurbani.sqlite', true);
-xhr.responseType = 'arraybuffer';
-
-xhr.onload = function(e) {
-  var uInt8Array = new Uint8Array(this.response);
-  db = new SQL.Database(uInt8Array);
-};
-xhr.send();
-
 var $search = document.getElementById("search");
 var $shabad = document.getElementById("shabad");
 var $results = document.getElementById("results");
 var $session = document.getElementById("session");
+var $buttons = document.getElementById("buttons");
 
 $search.addEventListener("keyup", typeSearch);
+$shabad.addEventListener("click", clickShabad);
 $results.addEventListener("click", clickResult);
 $session.addEventListener("click", clickSession);
+$buttons.addEventListener("click", clickButtons);
 
 function typeSearch() {
   clearTimeout(new_search_timeout);
@@ -28,17 +21,21 @@ function typeSearch() {
 function search() {
   var search_query = $search.value;
   if (search_query.length > 2) {
-    var content = db.exec("SELECT ID, Gurmukhi, ShabadID FROM Shabad WHERE FirstLetters LIKE '%" + search_query + "%'");
-    if (content.length > 0) {
-      $results.innerHTML = "";
-      content[0].values.forEach(function(item, i) {
-        $results.innerHTML = $results.innerHTML + "<li><a href='#' class='panktee' data-shabad-id='" + item[2] + "'>" + item[1] + "</a></li>";
-      });
-    } else {
-      $results.innerHTML = "<li class='english'>No results.</li>";
-    }
+    getResults(search_query, parseResults);
   } else {
     $results.innerHTML = "";
+  }
+}
+
+function parseResults(results) {
+  var content = JSON.parse(results);
+  if (content.length > 0) {
+    $results.innerHTML = "";
+    content.forEach(function(item, i) {
+      $results.innerHTML = $results.innerHTML + "<li><a href='#' class='panktee' data-shabad-id='" + item.ShabadID + "'>" + item.Gurmukhi + "</a></li>";
+    });
+  } else {
+    $results.innerHTML = "<li class='english'>No results.</li>";
   }
 }
 
@@ -46,6 +43,7 @@ function clickResult(e) {
   if (e.target.classList.contains("panktee")) {
     var $panktee = e.target;
     var ShabadID = $panktee.dataset.shabadId;
+    console.log(ShabadID);
     $session.innerHTML = $session.innerHTML + '<li><a href="#" class="panktee" data-shabad-id="' + ShabadID + '">' + $panktee.innerText + '</a></li>';
     loadShabad(ShabadID);
   }
@@ -59,12 +57,53 @@ function clickSession(e) {
   }
 }
 
-function loadShabad(ShabadID) {
-  var content = db.exec("SELECT ID, Gurmukhi FROM Shabad WHERE ShabadID = '" + ShabadID + "'");
+function writeShabadLines(results) {
+  var content = JSON.parse(results);
   if (content.length > 0) {
     $shabad.innerHTML = "";
-    content[0].values.forEach(function(item, i) {
-      $shabad.innerHTML = $shabad.innerHTML + '<li><a href="#" class="panktee">' + item[1] + '</a></li>';
+    content.forEach(function(item, i) {
+      $shabad.innerHTML = $shabad.innerHTML + '<li><a href="#" class="panktee">' + item.Gurmukhi + '</a></li>';
     });
   }
+}
+
+function clickShabad(e) {
+  if (e.target.classList.contains("panktee")) {
+    var $panktee = e.target;
+    sendText($panktee.innerText);
+  }
+}
+
+function clickButtons(e) {
+  if (e.target.classList.contains("msg")) {
+    var $msg = e.target;
+    sendText($msg.innerText);
+  }
+}
+
+if (typeof getResults !== 'function') {
+  function getResults(search_query, callback) {
+    localRequest("search=" + search_query, callback);
+  }
+}
+
+if (typeof loadShabad !== 'function') {
+  function loadShabad(ShabadID) {
+    localRequest("shabad=" + ShabadID, writeShabadLines);
+  }
+}
+
+function localRequest(query, callback) {
+  var request = new XMLHttpRequest();
+  request.open('GET', "http://127.0.0.1/bani.php?" + query, true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      if (typeof callback == "function") {
+        callback(this.response);
+      }
+    }
+  };
+  request.send();
 }
