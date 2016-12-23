e8b8ae4be5ff55e08c98f40f48c9eaae3e35337e
Navdeep Singh <info@cyfersystems.com>
2016-12-23 15:36:09 -0800

    Prevent session list from duplicating Shabads Use better-sqlite3 Send Panktee when clicking on search result Highlight current Panktee in Shabad controller

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var db,
     new_search_timeout,
-    electron  = false,
-    cordova   = false,
-    dbPath    = "./",
-    storage;
+    electron    = false,
+    cordova     = false,
+    dbPath      = "./",
+    storage,
+    sessionList = [];
 var sources = {
   "G": "Guru Granth Sahib",
   "D": "Dasam Granth Sahib",
@@ -96,18 +97,16 @@ function search() {
       db_query = db_query.substr(0, db_query.length - 1) + '%';
   }
   if (search_query.length > 2) {
-    db.serialize(function() {
-      db.all("SELECT _id, gurmukhi, shabad_no, source_id, ang_id, writer_id, raag_id FROM shabad WHERE " + search_col + " LIKE '" + db_query + "'", function(err, rows) {
-        if (rows.length > 0) {
-          $results.innerHTML = "";
-          rows.forEach(function(item, i) {
-            $results.innerHTML = $results.innerHTML + "<li><a href='#' class='panktee' data-shabad-id='" + item.shabad_no + "' data-line-id='" + item._id + "'><span class='result gurmukhi'>" + item.gurmukhi + "</span><span class='meta english'>" + sources[item.source_id] + " - " + item.ang_id + "</span></a></li>";
-          });
-        } else {
-         $results.innerHTML = "<li class='english'><span>No results</span></li>";
-        }
+    var stmt = db.prepare("SELECT _id, gurmukhi, shabad_no, source_id, ang_id, writer_id, raag_id FROM shabad WHERE " + search_col + " LIKE '" + db_query + "'");
+    var rows = stmt.all();
+    if (rows.length > 0) {
+      $results.innerHTML = "";
+      rows.forEach(function(item, i) {
+        $results.innerHTML = $results.innerHTML + "<li><a href='#' class='panktee' data-shabad-id='" + item.shabad_no + "' data-line-id='" + item._id + "'><span class='result gurmukhi'>" + item.gurmukhi + "</span><span class='meta english'>" + sources[item.source_id] + " - " + item.ang_id + "</span></a></li>";
       });
-    });
+    } else {
+     $results.innerHTML = "<li class='english'><span>No results</span></li>";
+    }
   } else {
     $results.innerHTML = "";
   }
@@ -118,7 +117,17 @@ function clickResult(e) {
     var $panktee  = (e.target.tagName == "A" ? e.target : e.target.parentElement);
     var ShabadID  = $panktee.dataset.shabadId;
     var LineID    = $panktee.dataset.lineId;
-    $session.innerHTML = $session.innerHTML + '<li><a href="#" class="panktee" data-shabad-id="' + ShabadID + '" data-line-id="' + LineID + '">' + $panktee.children[0].innerText + '</a></li>';
+    if (sessionList.indexOf(ShabadID) < 0) {
+      $session.innerHTML = $session.innerHTML + '<li><a href="#" class="panktee" data-shabad-id="' + ShabadID + '" data-line-id="' + LineID + '">' + $panktee.children[0].innerText + '</a></li>';
+      sessionList.push(ShabadID);
+    } else {
+      var line = $session.querySelector("[data-shabad-id='" + ShabadID + "']");
+      if (line.dataset.lineId != LineID) {
+        line.dataset.lineId = LineID;
+        line.innerText = $panktee.children[0].innerText;
+      }
+    }
+    sendLine(LineID);
     loadShabad(ShabadID, LineID);
   }
 }
@@ -133,22 +142,27 @@ function clickSession(e) {
 }
 
 function loadShabad(ShabadID, LineID) {
-  db.serialize(function() {
-    db.all("SELECT _id, gurmukhi FROM shabad WHERE shabad_no = '" + ShabadID + "'", function(err, rows) {
-      if (rows.length > 0) {
-        $shabad.innerHTML = "";
-        rows.forEach(function(item, i) {
-          $shabad.innerHTML = $shabad.innerHTML + '<li><a href="#" class="panktee' + (LineID == item._id ? ' current' : '') + '" data-line-id="' + item._id + '">' + item.gurmukhi + '</a></li>';
-        });
-      }
-    })
-  });
+  var stmt = db.prepare("SELECT _id, gurmukhi FROM shabad WHERE shabad_no = '" + ShabadID + "'");
+  var rows = stmt.all();
+  if (rows.length > 0) {
+    $shabad.innerHTML = "";
+    rows.forEach(function(item, i) {
+      $shabad.innerHTML = $shabad.innerHTML + '<li><a href="#" class="panktee' + (LineID == item._id ? ' current' : '') + '" data-line-id="' + item._id + '">' + item.gurmukhi + '</a></li>';
+    });
+  }
 }
 
 function clickShabad(e) {
   if (e.target.classList.contains("panktee")) {
     var $panktee = e.target;
     sendLine($panktee.dataset.lineId);
+    //Remove 'current' class from all Panktees
+    var lines = $shabad.querySelectorAll("a.panktee");
+    Array.prototype.forEach.call(lines, function(el, i){
+      el.classList.remove("current");
+    });
+    //Add 'current' to selected Panktee
+    $panktee.classList.add("current");
   }
 }
 
