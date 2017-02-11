5b4436945731462aee5db16fe41b7063b816a979
Navdeep Singh <me@navdeepsingh.ca>
2017-02-10 18:50:00 -0800

    Move search bar to top only if a search is performed

--- js/main.js
+++ js/main.js
@@ -61,7 +61,6 @@ if (scripts) {
 }
 
 function typeSearch() {
-  document.body.className = document.body.className.replace("home","");
   clearTimeout(new_search_timeout);
   new_search_timeout = setTimeout(search, 500);
 }
@@ -82,6 +81,7 @@ function search() {
   //Strip trailing comma and add a wildcard
   db_query = db_query.substr(0, db_query.length - 1) + '%';
   if (search_query.length > 2) {
+  document.body.classList.remove("home");
     db.all("SELECT s._id, s.gurmukhi, s.english_ssk, s.english_bms, s.transliteration, s.shabad_no, s.source_id, s.ang_id, w.writer_english AS writer, r.raag_english AS raag FROM shabad s JOIN writer w ON s.writer_id = w._id JOIN raag r ON s.raag_id = r._id WHERE " + search_col + " LIKE '" + db_query + "'", function(err, rows) {
       if (rows.length > 0) {
         $results.innerHTML = "";
