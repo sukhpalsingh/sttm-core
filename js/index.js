2dc066d8239cff3c3f5997e43e11e144fde4c379
Navdeep Singh <me@navdeepsingh.ca>
2018-02-16 12:27:03 -0800

    Add JSDoc to platformMethod

--- js/index.js
+++ js/index.js
@@ -58,6 +58,16 @@ if (typeof Mousetrap !== 'undefined') {
   Mousetrap.bind('space', spaceBar);
 }
 
+/**
+ * Check if the platform has a method and call if it is does
+ *
+ * @since 3.2.2
+ * @param {string} method Name of the platform method
+ * @param {any} args Arguments to be passed to the method
+ * @example
+ *
+ * global.core.platformMethod('updateSettings');
+ */
 function platformMethod(method, args) {
   if (typeof global.platform[method] === 'function') {
     global.platform[method](args);
